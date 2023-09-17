7bad62dfcdfe4d567e274659e798f28bef482c31
Hsi-Huang Tseng <9501sam@gmail.com>
2023-09-17 18:35:13 +0800

    add user program `sleep`

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char **argv)
+{
+  uint ticks;
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep <ticks>\n");
+    exit(1);
+  }
+  ticks = atoi(argv[1]);
+
+  sleep(ticks);
+
+  exit(0);
+}
